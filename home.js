f938be544a6fff427d1beca6210d419ddd99841f
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-10-04 16:11:10 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const Home = () =>{
+    return(
+        <div>
+            <img className="homeimg" src="https://images.unsplash.com/photo-1584651766658-1b7b47a2cb79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
+            <div className="why-coding">
+                 <h1>Why Coding At Young Age?</h1>
+            </div> 
+            <div className="row">
+                                <div className="column">
+                                    <div className="card">
+                                    <h3>Card 1</h3>
+                                    <p>Some text</p>
+                                    <p>Some text</p>
+                                    </div>
+                                </div>
+
+                                <div className="column">
+                                    <div className="card">
+                                    <h3>Card 2</h3>
+                                    <p>Some text</p>
+                                    <p>Some text</p>
+                                    </div>
+                                </div>
+                                
+                                <div className="column">
+                                    <div className="card">
+                                    <h3>Card 3</h3>
+                                    <p>Some text</p>
+                                    <p>Some text</p>
+                                    </div>
+                                </div>
+                                
+                                <div className="column">
+                                    <div className="card">
+                                    <h3>Card 4</h3>
+                                    <p>Some text</p>
+                                    <p>Some text</p>
+                                    </div>
+                                </div>
+                                </div>
+            <div className="why-coding">
+                 <h1>Why Coding With Us?</h1>
+            </div>    
+            <div className="row">
+                                <div className="column">
+                                    <div className="card">
+                                    <h3>Card 1</h3>
+                                    <p>Some text</p>
+                                    <p>Some text</p>
+                                    </div>
+                                </div>
+
+                                <div className="column">
+                                    <div className="card">
+                                    <h3>Card 2</h3>
+                                    <p>Some text</p>
+                                    <p>Some text</p>
+                                    </div>
+                                </div>
+                                
+                                <div className="column">
+                                    <div className="card">
+                                    <h3>Card 3</h3>
+                                    <p>Some text</p>
+                                    <p>Some text</p>
+                                    </div>
+                                </div>
+                                
+                                <div className="column">
+                                    <div className="card">
+                                    <h3>Card 4</h3>
+                                    <p>Some text</p>
+                                    <p>Some text</p>
+                                    </div>
+                                </div>
+                                </div>             
+        </div>
+    )
+}
+
+export default Home;
